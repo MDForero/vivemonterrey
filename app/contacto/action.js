6b54a43e06f803275fa282bb08e42545f4d5aeda
Michael Forero <mforeroroldan@gmail.com>
2024-10-17 22:57:24 -0500

    recaptcha implementado primera prueba de funcionamiento

--- app/contacto/action.js
+++ app/contacto/action.js
@@ -11,7 +11,7 @@ export default async function action(formData) {
                 email: formData.get('email'),
                 message: formData.get('message'),
                 tel: formData.get('tel'),
-                'g-recaptcha-response': '6LdohWQqAAAAAEgx2NgQO-AqevtV0dZwPSVt4Av2',
+                'g-recaptcha-response': formData.get('recaptcha'),
             },
             {
                 publicKey: 'eBXA-8jwUxc33exZ9',
--- components/FormContact.js
+++ components/FormContact.js
@@ -10,20 +10,22 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "./ui/textarea"
 import action from "@/app/contacto/action"
-import { useEffect } from "react"
+import { useState } from "react"
+import ReCAPTCHA from "react-google-recaptcha"
 
 export const description =
     "A sign up form with first name, last name, email and password inside a card. There's an option to sign up with GitHub and a link to login if you already have an account"
 
 export function FormContact() {
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://www.google.com/recaptcha/api.js';
-        script.async = true;
-        document.body.appendChild(script);
-    }, [])
 
+    const [recaptcha, setRecaptcha] = useState(null)
+
+    const handleReCaptchaChange = (token) => {
+        setRecaptcha(token)
+        alert("Captcha value:", token)
+    }
     return (
+
         <Card className="mx-auto max-w-2xl w-full">
             <CardHeader>
                 <CardTitle className="text-xl">Contáctenos  </CardTitle>
@@ -32,6 +34,7 @@ export function FormContact() {
             <CardContent>
                 <form action="#" method="POST" className="grid gap-4">
                     <div className="grid grid-cols-2 gap-4">
+                        <Input type="hidden" name="recaptcha" value={recaptcha} />
                         <div className="grid gap-2 col-span-2">
                             <Label htmlFor="name">Nombre</Label>
                             <Input id="name" name='name' placeholder="Juan Perez" required />
@@ -55,12 +58,17 @@ export function FormContact() {
                         <Label htmlFor="message">Mensaje</Label>
                         <Textarea name='message' placeholder="Aquí puedes dejar tus sugerencias, inquietudes y demás." id="message" className='h-1/2' />
                     </div>
-                    <div className="g-recaptcha mx-auto" data-sitekey='6LdohWQqAAAAAEgx2NgQO-AqevtV0dZwPSVt4Av2'></div>
+                    <ReCAPTCHA
+                        sitekey="6LdohWQqAAAAAEgx2NgQO-AqevtV0dZwPSVt4Av2"
+                        onChange={handleReCaptchaChange}
+                        className="mx-auto"
+                    />
                     <Button formAction={action} className="w-full bg-[#b91c1c]">
                         Contactar
                     </Button>
                 </form>
             </CardContent>
         </Card>
+
     )
 }
