8534f5b01a8dc6ac3b8f70351dcb2d22cd4d6552
Michael Forero <mforeroroldan@gmail.com>
2024-12-04 17:23:55 -0500

    Añadir componente ScrollArea y actualizar dependencias de Radix UI; se elimina el SidebarProvider en el layout del menú.

--- app/[categoria]/[negocio]/menu/layout.js
+++ app/[categoria]/[negocio]/menu/layout.js
@@ -6,10 +6,7 @@ import { CartProvider } from "./CartContext";
 export default function MenuLayout({ children }) {
     return <div className="relative">
         <CartProvider>
-            <SidebarProvider style={{ '--sidebar-width': '20rem' }}>
-
-                        {children}
-            </SidebarProvider>
+            {children}
         </CartProvider>
     </div>
 
--- app/[categoria]/[negocio]/menu/page.js
+++ app/[categoria]/[negocio]/menu/page.js
@@ -5,14 +5,14 @@ import { useEffect, useState } from "react"
 import CardProducts from "@/components/CardProducts"
 import ImageSupabase from "@/components/ImageSupabase"
 import { actions, useCart, useCartDispatch } from "./CartContext"
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader, SidebarInset, SidebarMenu, SidebarRail, useSidebar } from "@/components/ui/sidebar"
 import { Table, TableBody, TableCell, TableHeader, TableRow } from "@/components/ui/table"
-
 import { Button } from "@/components/ui/button"
 import { ShoppingCart, Trash2Icon } from "lucide-react"
 import Link from "next/link"
 import Image from "next/image"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet"
 
 
 export default function Page({ params }) {
@@ -45,78 +45,78 @@ export default function Page({ params }) {
     }, [params])
 
 
-    return <>
-        <TriggerShopping />
-        <SidebarInset>
-            {business && <ImageSupabase buckets={'banners'} url={business.logo} className='w-44 h-44 aspect-video object-cover rounded-full'/> }
+    return <><div>
+        {business && <ImageSupabase buckets={'banners'} url={business.logo} className='w-44 h-44 aspect-video object-cover rounded-full' />}
+        <div className="container">
+
             {business?.categories_restaurant && <Tabs defaultValue={business?.categories_restaurant[0]}>
-                <TabsList className=' gap-2 bg-white z-0'>
-                    {business?.categories_restaurant.map(category => <TabsTrigger className='data-[state=active]:text-white data-[state=active]:bg-[#b91c1c] border bg-blue-100' key={category} value={category}>{category}</TabsTrigger>)}
-                </TabsList>
-                {business?.categories_restaurant.map(category => <TabsContent className='grid grid-cols-1 md:grid-cols-2 mx-auto w-fit lg:gap-12' key={category} value={category}>
+                <ScrollArea className='w-screen py-3'>
+                    <TabsList className=' gap-2 bg-white z-0'>
+                        {business?.categories_restaurant.map(category => <TabsTrigger className='data-[state=active]:text-white data-[state=active]:bg-[#b91c1c] border bg-blue-100' key={category} value={category}>{category}</TabsTrigger>)}
+                    </TabsList>
+                    <ScrollBar orientation='horizontal' />
+                </ScrollArea>
+                {business?.categories_restaurant.map(category => <TabsContent className='grid grid-cols-1 md:grid-cols-2 mx-auto w-fit md:gap-12' key={category} value={category}>
                     {products?.filter(product => product.category === category).map(product => <CardProducts key={product.id} product={product} dispatch={dispatch} action={actions.add} />)}
                 </TabsContent>)}
             </Tabs>}
-        </SidebarInset>
-        <AppSidebar />
+        </div>
+        <AppSheet />
 
+    </div>
     </>
 }
 
-const AppSidebar = () => {
-    const { state } = useSidebar()
+const AppSheet = () => {
     const dispatch = useCartDispatch()
     const cart = useCart()
-    return <Sidebar side='right' collapsible='offcanvas' variant='icon' className={(state === 'collapsed' ? 'h-0 w-0 ' : 'h-fit ') + 'absolute'} >
-        <SidebarHeader>
-            {!(cart.length === 0) ? <SidebarGroup className='font-bold'>Subtotal: {cart.map(item => item.quantity * item.price).reduce((acc, value) => acc + value, 0)} COP</SidebarGroup> : <></>}
-        </SidebarHeader>
-        <SidebarContent>
-            <SidebarGroup>
-                <SidebarMenu>
-                    <div className={(state === 'collapsed' ? 'hidden' : '')}>
-                        <div className="space-y-2">
-                            {!(cart.length === 0) ? <></> : <SidebarGroup className='font-bold'>El carrito esta vacio</SidebarGroup>}
-                            <Table>
-                                <TableHeader>
-                                    <TableRow className='font-bold'>
-                                        <TableCell>Imagen</TableCell>
-                                        <TableCell>Productos</TableCell>
-                                        <TableCell>Precio</TableCell>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
+    return <Sheet >
+        <TriggerShopping />
+        <SheetContent className="w-[400px] sm:w-[90%] md:w-[540px]">
+            <SheetHeader>
+                {!(cart.length === 0) ? <SheetTitle className='font-bold'>Subtotal: {cart.map(item => item.quantity * item.price).reduce((acc, value) => acc + value, 0)} COP</SheetTitle> : <></>}
+            </SheetHeader>
+            <SheetDescription>
+                <div >
+                    <div className="space-y-2">
+                        {!(cart.length === 0) ? <></> : <SheetTitle className='font-bold'>El carrito esta vacio</SheetTitle>}
+                        <Table>
+                            <TableHeader>
+                                <TableRow className='font-bold'>
+                                    <TableCell>Imagen</TableCell>
+                                    <TableCell>Productos</TableCell>
+                                    <TableCell>Precio</TableCell>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
 
-                                    {cart?.map(item => <TableRow key={item.id} >
-                                        <TableCell><Image src={item.image} alt={item.name} width={50} height={50} className="rounded-full aspect-square object-cover" /></TableCell>
-                                        <TableCell className=" h-fit text-sm text-left w-36"><strong>{item.name.split(' ')[0].slice(0, 1) + ' ' + item.name.split(' ').slice(1, -1).join(' ')}</strong> x{item.quantity}</TableCell>
-                                        <TableCell className="text-left font-bold">{item.price * item.quantity}</TableCell>
-                                        <TableCell className="text-left font-bold w-4"><Button className='p-1' onClick={() => {
-                                            dispatch({
-                                                type: actions.remove, payload: { id: item.id }
-                                            })
-                                        }}><Trash2Icon /></Button></TableCell>
-                                    </TableRow>)}
-                                </TableBody>
-                            </Table>
-                        </div>
+                                {cart?.map(item => <TableRow key={item.id} >
+                                    <TableCell><Image src={item.image} alt={item.name} width={50} height={50} className="rounded-full aspect-square object-cover" /></TableCell>
+                                    <TableCell className=" h-fit text-sm text-left w-36"><strong>{item.name.split(' ')[0].slice(0, 1) + ' ' + item.name.split(' ').slice(1, -1).join(' ')}</strong> x{item.quantity}</TableCell>
+                                    <TableCell className="text-left font-bold">{item.price * item.quantity}</TableCell>
+                                    <TableCell className="text-left font-bold w-4"><Button className='p-1' onClick={() => {
+                                        dispatch({
+                                            type: actions.remove, payload: { id: item.id }
+                                        })
+                                    }}><Trash2Icon /></Button></TableCell>
+                                </TableRow>)}
+                            </TableBody>
+                        </Table>
                     </div>
-                </SidebarMenu>
-            </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter>
-            <div className={(state === 'collapsed' ? 'hidden' : '')}>
-                {!(cart.length === 0) ? <Link href='ordenar' asChild><Button className="btn btn-primary float-right" >Pagar</Button></Link> : <></>}
-                {!(cart.length === 0) ? <Button onClick={() => dispatch({ type: actions.clear })} className="float-left" variant='outline'>Limpiar orden</Button> : <></>}
-            </div>
-        </SidebarFooter>
-        <SidebarRail />
-    </Sidebar>
+                </div>
+            </SheetDescription>
+            <SheetFooter>
+                <div >
+                    {!(cart.length === 0) ? <Link href='ordenar' asChild><Button className="btn btn-primary float-right" >Pagar</Button></Link> : <></>}
+                    {!(cart.length === 0) ? <Button onClick={() => dispatch({ type: actions.clear })} className="float-left" variant='outline'>Limpiar orden</Button> : <></>}
+                </div>
+            </SheetFooter>
+        </SheetContent>
+    </Sheet >
 }
 
 const TriggerShopping = () => {
     const cart = useCart()
-    const { toggleSidebar } = useSidebar()
     const [animate, setAnimate] = useState(false)
 
     useEffect(() => {
@@ -127,5 +127,24 @@ const TriggerShopping = () => {
             }, 1000)
         }
     }, [cart])
-    return <Button onClick={toggleSidebar} className='absolute right-0 z-50'><div className="relative"> <ShoppingCart /><div className={(animate ? 'animate-ping text-2xl duration-1000' : ' ') + " absolute -top-3 -left-3 font-bold "}>{cart.reduce((acc, value) => acc + value.quantity, 0)}</div></div></Button>
+    return <>
+        <SheetTrigger asChild>
+            <Button  className='hidden md:block md:absolute top-0  right-0 z-50 '>
+                <p className="relative ">  <ShoppingCart className="hidden md:block" />
+                    <span className={(animate ? 'animate-ping text-2xl duration-1000' : ' ') + " md:absolute md:-top-3 md:-left-3 font-bold  "}>{cart.reduce((acc, value) => acc + value.quantity, 0)}</span>
+                    <strong className='md:hidden'> Ordenar </strong>
+                    <span className="md:hidden">{cart.reduce((acc, value) => acc + (value.quantity * value.price), 0)}</span>
+                </p>
+                </Button>
+        </SheetTrigger>
+        <SheetTrigger asChild>
+            <Button  className='fixed bottom-3  left-1 right-1  md:hidden md:top-0 md:left-[96%] md:right-0 z-50 '>
+                <p className="md:relative ">  <ShoppingCart className="hidden md:block" />
+                    <span className={(animate ? 'animate-ping text-2xl duration-1000' : ' ') + " md:absolute md:-top-3 md:-left-3 font-bold  "}>{cart.reduce((acc, value) => acc + value.quantity, 0)}</span>
+                    <strong className='md:hidden'> Ordenar </strong>
+                    <span className="md:hidden">{cart.reduce((acc, value) => acc + (value.quantity * value.price), 0)}</span>
+                </p>
+                </Button>
+        </SheetTrigger>
+    </>
 }
