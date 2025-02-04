046fb63c21bb2bcecc23f3299e4e456c4d4209eb
Michael Forero <mforeroroldan@gmail.com>
2025-02-03 19:09:57 -0500

    mejorar la presentación de la página "Qué hacer" con un diseño más limpio y carga de imágenes desde Supabase.

--- app/que-hacer/page.js
+++ app/que-hacer/page.js
@@ -1,26 +1,43 @@
 import CardBusinesses from "@/components/CardBusinesses"
+import ImageSupabase from "@/components/ImageSupabase"
+import { Separator } from "@/components/ui/separator"
 import { createClient } from "@/utils/supabase/server"
 import Image from "next/image"
+
+export const metadata = {
+    title: "¿Qué hacer en Monterrey Casanare?",
+    description: "Encuentra todo lo que Monterrey Casaanre tiene para ofrecerte. En Vive Monterrey podras encontrar toda la información necesaria para disfrutar de Monterrey Casanare, desde los mejores restaurantes, hoteles, sitios turisticos y mucho más",
+    keywords: "Monterrey, Casanare, Vive Monterrey, Llanos Orientales, Colombia, Turismo, Naturaleza, Aventura, Cultura Llanera, Gastronomía, Tradición, Ríos, Cascadas, Lagunas, Piscinas Naturales, Paratrike, Vuelos, Ecuestres, Miradores, Experiencias Agrarias, Senderos, Muestras Culturales, Biodiversidad, Llanuras, Llanero, ViveMonterrey, Vive Monterrey, ViveMonterrey.com, Vive Monterrey.com, ViveMonterrey.co, Vive Monterrey.co",
+}
 
 export default async function page() {
     const supabase = createClient()
     const { data, error } = await supabase.from('businesses').select('*, categories(name)')
+
     if (error) {
         return <div className="container mx-auto">
             <h1>Error al cargar la página</h1>
         </div>
     }
-    console.log(data[0].categories)
-    return <div className=" container mx-auto flex flex-col justify-center space-y-16 items-center bg-gray-50 ">
+
+    return <div className="bg-gray-50 space-y-8">
         <main>
-            <Image loading="lazy" src='/assets/portada-que-hacer.webp' alt="Imagen de portada que hacer en monterrey" width={0} height={0} className="w-full" />
+            <figure className="w-full h-[380px] md:h-[420px] lg:h-[600px]  overflow-hidden">
+                <ImageSupabase url={'monterrey.png'} buckets={'assets'} alt="Imagen de portada que hacer en monterrey" className=" w-full sm:h-[380px] md:h-[420px] lg:h-[600px] object-cover" />
+            </figure>
         </main>
-        <div className=" md:flex flex-col justify-center items-center  font-light">
-            <h1 className="text-center text-[#b91c1c] text-pretty text-3xl md:text-4xl lg:text-5xl font-semibold ">¿Qué hacer en Monterrey Casanare?</h1>
-            <p className="text-center text-lg md:text-xl  max-w-5xl p-2">Encuentra todo lo que Monterrey Casanare tiene para ofrecerte. En Vive Monterrey podras encontrar toda la información necesaria para disfrutar de Monterrey Casanare, desde los mejores restaurantes, hoteles, sitios turisticos y mucho más</p>
+        <div className=" container mx-auto flex flex-col justify-center space-y-16 items-center ">
+
+            <div className=" md:flex flex-col justify-center space items-center  font-light">
+                <div className="space-y-4">
+                <h1 className="text-center text-[#b91c1c] text-pretty text-3xl md:text-4xl lg:text-5xl font-semibold ">¿Qué hacer en Monterrey Casanare?</h1>
+                <Separator className='border-2 border-viveRed/60 rounded-full '/>
+                </div>
+                <p className="text-center text-lg md:text-xl  max-w-5xl p-2">Encuentra todo lo que Monterrey Casanare tiene para ofrecerte. En Vive Monterrey podras encontrar toda la información necesaria para disfrutar de Monterrey Casanare, desde los mejores restaurantes, hoteles, sitios turisticos y mucho más</p>
+            </div>
+            <section className="flex justify-evenly w-full flex-wrap py-8 gap-8 px-2 mx-1">
+                {data.map((business) => <CardBusinesses key={business.id} data={business} />)}
+            </section>
         </div>
-        <section className="flex justify-evenly w-full flex-wrap py-8 gap-8 px-2 mx-1">
-            {data.map((business) => <CardBusinesses key={business.id} data={business} />)}
-        </section>
     </div>
 }
--- components/ImageSupabase.js
+++ components/ImageSupabase.js
@@ -2,21 +2,26 @@
 import React, { useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
 import Image from 'next/image'
+import { set } from 'date-fns'
 
 
-export default function  ImageSupabase({buckets, url, className, alt}) {
+export default function ImageSupabase({ buckets, url, className, alt }) {
     const [imageUrl, setImageUrl] = useState(null)
+    const [loading, setLoading] = useState(true)
     const supabase = createClient()
     console.log(url)
     useEffect(() => {
-       async function downloadImage (){
+        async function downloadImage() {
             try {
+
                 const { data, error } = await supabase.storage.from(buckets).download(url)
                 if (error) {
+                    console.log('Error downloading image: ', error)
                     throw error
                 }
                 const image = URL.createObjectURL(data)
                 setImageUrl(image)
+                setLoading(false)
             } catch (error) {
                 console.log('Error downloading image: ', error)
             }
@@ -24,8 +29,9 @@ export default function  ImageSupabase({buckets, url, className, alt}) {
         downloadImage()
     }, [supabase])
 
-    return (<>
-        <Image src={imageUrl ?? url} width={0} alt={alt ?? 'Imagen de vive monterrey'} height={0} className={className} />
+    return (<> 
+            {!loading ? <Image src={imageUrl ?? url} width={0} alt={alt ?? 'Imagen de vive monterrey'} height={0} className={className} /> : <div className={className + ' bg-gray-50 flex justify-center items-center'}>Loading...</div>}
+            {imageUrl}
     </>
     )
 }
