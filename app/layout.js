f2db8de1815d4d58dcd81fbd4ac978e02afa5840
Michael Forero <mforeroroldan@gmail.com>
2025-02-16 19:34:31 -0500

    feat: mejorar la estructura del componente RootLayout para mayor claridad y legibilidad

--- app/layout.js
+++ app/layout.js
@@ -34,28 +34,28 @@ export default function RootLayout({ children }) {
     { name: 'Vivir en Monterrey', url: '/vivir-en-monterrey' },
     { name: 'Contacto', url: '/contacto' },
   ]
-  
-    return (
-      <html lang="es" className={`${asap.variable} ${englebert.variable}`}>
-        <body >
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-M2MJPJS6N6" />
-          <Script id="google-analytics">
-            {`
+
+  return (
+    <html lang="es" className={`${asap.variable} ${englebert.variable}`}>
+      <body >
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-M2MJPJS6N6" />
+        <Script id="google-analytics">
+          {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
           gtag('config', 'G-M2MJPJS6N6');
         `}
-          </Script>
+        </Script>
 
-          <NavBar links={links} />
-          <BackTopButton />
-          <div className="container mx-auto lg:space-y-52 md:space-y-24  space-y-12 mb-20">
-            {children}
-          </div>
-          <Footer />
-        </body>
-      </html>
-    )
+        <NavBar links={links} />
+        <BackTopButton />
+        <div className="container mx-auto lg:space-y-52 md:space-y-24  space-y-12 mb-20">
+          {children}
+        </div>
+        <Footer />
+      </body>
+    </html>
+  )
 }
