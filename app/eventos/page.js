a97a010a9a79a50c178f414317577822a3401de4
Michael Forero <mforeroroldan@gmail.com>
2025-02-16 19:35:01 -0500

    feat: implementar la carga y visualización de eventos en la página de eventos

--- app/eventos/page.js
+++ app/eventos/page.js
@@ -1,9 +1,19 @@
+import CardEvent from "@/components/CardEvent"
+import { createClient } from "@/utils/supabase/server"
 export const metadata = {
     title: "Eventos",
     description: "Encuentra los eventos mas importantes de Monterrey Casanare.",
     keywords: "Eventos en Monterrey, Eventos Monterrey Casanare",
     image: "/assets/portada-que-hacer.webp",
 }
-export default function Page() {
-    return<></>
+export default async function page() {
+    const supabase = createClient()
+    const dateNow = new Date()
+    const date = dateNow.toISOString()
+    const nextMonthDate = new Date(dateNow.setMonth(dateNow.getMonth() + 1)).toISOString()
+    const { data: events, error } = await supabase.from('events').select('*').gte('event_date', date).lte('event_date', nextMonthDate)
+    console.log(events, "events", error, "error")
+    return <>
+        {events && events.map((event, index) => <CardEvent key={index} event={event} />)}
+    </>
 }
