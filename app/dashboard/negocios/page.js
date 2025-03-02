353c43c349598a9960d16eaf50a5bd3a126ac83c
Michael Forero <mforeroroldan@gmail.com>
2025-03-02 10:23:22 -0500

    feat: agregar funcionalidad para registrar un negocio con carga de imágenes y horarios

--- app/dashboard/negocios/page.js
+++ app/dashboard/negocios/page.js
@@ -1,9 +1,12 @@
 import CardBusinesses from "@/components/layouts/dashboard/CardBusinesses"
 import PaginationBusinessesAdmin from "@/components/PaginationBusinessesAdmin"
+import { HousePlusIcon } from "lucide-react"
+import Link from "next/link"
 
 export default function Page() {
     return (
         <div className="max-w-7xl mx-auto">
+            <Link href='/dashboard/negocios/registrar-negocio' className="bg-viveRed block w-fit mx-auto p-2 rounded-md"><HousePlusIcon size={54} color="white"/></Link>
             <PaginationBusinessesAdmin />
         </div>
     )
