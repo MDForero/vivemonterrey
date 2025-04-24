1e822471dd28880d08630ef46fd718c301bd0769
Michael <mforeroroldan@gmail.com>
2025-04-24 15:39:49 -0500

    Refactor CardCategory component layout and styling for improved responsiveness and consistency

--- components/index/CardCategory.js
+++ components/index/CardCategory.js
@@ -7,24 +7,24 @@ const CardCategory = ({ data }) => {
 
 
 
-  return (<div key={data.name} className="col-xxl-3 col-xl-4 col-sm-6">
+  return (<div key={data.name} className="basis-1/2 sm:basis-1/2 md:basis-1/4  p-2">
     <div
-      className="destination-item style-two bgc-lighter"
+      className="destination-item style-two bgc-lighter p-2"
       data-aos="flip-up"
       data-aos-duration={1500}
       data-aos-offset={50}
     >
-      <div className="relative w-full  p-2 aspect-[4/3] object-cover ">
+      <Link href={`/${data.name.split(' ').join('-')}`} className=" relative w-full aspect-[9/8] block overflow-hidden rounded-md">
         <ImageSupabase url={data.image_url} buckets={'categories_image'} alt="Destination" className='w-full aspect-[9/8] object-cover rounded-md' />
-        <span className="absolute bottom-4 right-4 bg-white rounded-md px-[0.7rem] py-[0.1rem] text-[0.9rem] text-black flex gap-2 justify-center items-center">
-          <i className="far fa-map-marker-alt" /> {data.name}
+        <span className="sm:hidden absolute bottom-4 right-4 bg-white rounded-md px-[0.7rem] py-[0.1rem] text-[0.9rem] text-black flex gap-2 justify-center items-center">
+          <i className="far fa-map-marker-alt " /> {data.businesses.length}+ Lugares
         </span>
-      </div>
+      </Link>
       <div className="py-3 px-2">
         <h6>
           <Link href={`/${data.name.split(' ').join('-')}`}>{data.name}</Link>
         </h6>
-        <span className="time">{data.businesses.length}+ Lugares</span>
+        <span className="time ">{data.businesses.length}+ Lugares</span>
       </div>
     </div>
   </div>)
