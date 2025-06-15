550345d96d5c676247576c2370fec5e89159bc3e
Michael <mforeroroldan@gmail.com>
2025-06-14 20:49:38 -0500

    Agregar lógica para mostrar scripts condicionalmente según la ruta en el componente RootLayout

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+'use client'
 import "@css/aos.css";
 import "@css/bootstrap.min.css";
 import "@css/flaticon.min.css";
@@ -9,6 +10,8 @@ import "./globals.css";
 import "@css/style.css";
 import Script from "next/script";
 import ReveloLayout from "@/components/layouts/ReveloLayout";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 // import { createClient } from "@/utils/supabase/client";
 
 
@@ -17,13 +20,17 @@ import ReveloLayout from "@/components/layouts/ReveloLayout";
 
 export default function RootLayout({ children }) {
 
-  const links = [
-    { name: 'Inicio', url: '/' },
-    { name: '¿Que hacer?', url: '/explora' },
-    { name: 'Eventos', url: '/eventos' },
-    { name: 'Vivir en Monterrey', url: '/vivir-en-monterrey' },
-    { name: 'Contacto', url: '/contacto' },
-  ]
+  const path = usePathname()
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    // Elimina strings vacíos del pathname (e.g., "/")
+    const pathSegments = path.split('/').filter(Boolean);
+
+    // Mostrar el script solo si hay 2 o menos segmentos de ruta
+    setShow(pathSegments.length <= 1);
+    console.log(pathSegments, 'pathSegments', show)
+  }, [path])
 
   return (
     <html lang="es" className={``}>
@@ -41,8 +48,12 @@ export default function RootLayout({ children }) {
           gtag('config', 'G-M2MJPJS6N6');
         `}
         </Script>
-
-        <ReveloLayout>
+        {show && <Script >
+          {`
+      (function(a,m,o,c,r,m){a[m]={id:"1035903",hash:"cb04e816b91dcce78f3d3e4970d3b4165a95994b80b0ac4d9304772a6a471b58",locale:"es",setMeta:function(p){this.params=(this.params||[]).concat([p])}};a[o]=a[o]||function(){(a[o].q=a[o].q||[]).push(arguments)};var d=a.document,s=d.createElement('script');s.async=true;s.id=m+'_script';s.src='https://gso.kommo.com/js/button.js';d.head&&d.head.appendChild(s)}(window,0,'crmPlugin',0,0,'crm_plugin'));
+          `}
+        </Script>
+        }       <ReveloLayout>
           {children}
         </ReveloLayout>
       </body>
